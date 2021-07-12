edd229e018e354523c469b51d1f8afbc8be43eef
seojinsarejj <s19009@naver.com>
2021-07-12 09:30:55 +0900

    [UPDATE] Update return value

--- main.py
+++ main.py
@@ -8,14 +8,24 @@ app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def transfer():
-    content = request.files["content"]
-    style = request.files["style"]
-    wct_main(content,style)
 
-    with open('output.jpg', 'rb') as data:
-        url = s3upload(data, "test")
+    try:
 
-    return str(url)
+        content = request.files["content"]
+        style = request.files["style"]
+        wct_main(content,style)
+
+        with open('output.jpg', 'rb') as data:
+            url = s3upload(data, "test")
+
+        response_object = {"url":url}
+
+        return response_object,200
+        
+    except Exception as e:
+
+        response_object = {"status": "fail", "message": e}
+        return response_object,400
 
 if __name__ == '__main__':
     app.run(port=8000,debug=True)
